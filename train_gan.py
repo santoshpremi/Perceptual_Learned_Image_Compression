982fb15d250c8baaf09ef54fd65803e23c7cb793
santoshpremi <santoshadhikaripremi@gmail.com>
2026-01-07 21:24:16 +0100

    Clear memory before validation

--- train_gan.py
+++ train_gan.py
@@ -1,6 +1,7 @@
 import os
 import random
 import logging
+import gc
 from PIL import ImageFile, Image
 import math
 import torch
@@ -169,6 +170,10 @@ def main():
             current_step,
             config
         )
+
+        # Clear memory before validation
+        torch.cuda.empty_cache()
+        gc.collect()
 
         save_dir = os.path.join('./experiments', args.experiment, 'val_images', '%03d' % (epoch + 1))
         loss = test_one_epoch_gan(epoch, test_dataloader, net, net_disc, criterion, save_dir, logger_val, tb_logger, config)
